c60a7bb63ef1956d0304e4ad1807540a29c98d44
andrioskasara <andrioska.sara@gmail.com>
2025-07-18 19:25:23 +0200

    created User entity, UserRole enum and UserRepository interface

---
+++ backend/src/main/java/mk/ukim/finki/backend/model/entity/User.java
@@ -0,0 +1,49 @@
+package mk.ukim.finki.backend.model.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import mk.ukim.finki.backend.model.enums.UserRole;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id", updatable = false, nullable = false)
+    private UUID id;
+
+    @Column(nullable = false, unique = true, length = 255)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private UserRole role;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private Instant createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    private Instant updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = Instant.now();
+        updatedAt = createdAt;
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updatedAt = Instant.now();
+    }
+}
